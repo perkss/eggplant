6e8e68ee8b48e834feb1f48c2c216d540284a063
egg-plant <stuart.m.perks@gmail.com>
2017-05-09 19:17:14 +0100

    add in defSpecification function #7
    Define new keyword for def spec

--- src/eggplant/core.clj
+++ src/eggplant/core.clj
@@ -5,8 +5,8 @@
 (defn given [x]
   (var-get (def var x)))
 
-(defn when-we-process [testFn & data]
-  (apply testFn data))
+(defn when-we-process [test-fn & data]
+  (apply test-fn data))
 
 ;; TODO need to make this equal work for all types
 (defn then-we-expect [expected actual]
@@ -15,7 +15,12 @@
 (defn then-we-do-not-expect [expected actual]
   (is (false? (= expected actual))))
 
-;; TODO define a defSpecification fn that refers to defTest
+;; TODO define a defspecification fn that refers to deftest or testing
+(defmacro defspecification
+  [specName & testBody]
+    `(def ~(vary-meta specName assoc :test `(fn [] ~@testBody))
+       (fn [] (test-var (var ~specName)))))
+
 
 
 
