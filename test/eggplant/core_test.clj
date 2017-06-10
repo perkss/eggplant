782afee7ffa4f1238ced3d951a6e98b389e92a45
egg-plant <stuart.m.perks@gmail.com>
2017-06-10 16:31:36 +0100

    Feature Given Keyword.

--- test/eggplant/core_test.clj
+++ test/eggplant/core_test.clj
@@ -5,7 +5,7 @@
 
 (deftest give-test
   (testing "Check the when defines variable"
-    (is (= 2 (given 2)))))
+    (is (= 2 (given 2 test-data-name)))))
 
 (deftest when-we-process-test
   (testing "Check the when calls the function"
@@ -17,7 +17,7 @@
 
 (deftest give-when-we-process-test
   (testing "Check the when calls the function"
-    (is (= 4 (when-we-process + 2 (given 2))))))
+    (is (= 4 (when-we-process + 2 (given 2 test-data-name))))))
 
 (deftest then-we-expect-test-fail
   (testing "Test the then will fail on negatives"
@@ -25,18 +25,17 @@
 
 (deftest example-specification-true
   (testing "A full length example specification which is true 4 * 2 = 8"
-    (then-we-expect 8 (when-we-process * 2 (given 4)))))
+    (then-we-expect 8 (when-we-process * 2 (given 4 test-data-name)))))
 
 (deftest example-specification-false
   (testing "A full length example specification which is false 4 * 2 = 6 ! False"
-    (then-we-do-not-expect 6 (when-we-process * 2 (given 4)))))
+    (then-we-do-not-expect 6 (when-we-process * 2 (given 4 test-data-name)))))
 
 (defspec example-specification-with-strings
   (testing "String upper case specification"
-    (then-we-expect "TOM" (when-we-process str/upper-case (given "tom")))))
+    (then-we-expect "TOM" (when-we-process str/upper-case (given "tom" test-data-name)))))
 
 (defspec example-specification-with-thread-macro
   (testing "Test with Thread Macro"
-    (as-> 2 test-data
-          (given test-data)
-          (then-we-expect 4 (when-we-process * 2 test-data)))))
+    (given 2 test-data-name
+          (then-we-expect 4 (when-we-process * 2 test-data-name)))))
